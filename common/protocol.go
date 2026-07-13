2dd025c63601f203f2eaaf6c7f05ea9b53bf34a6
shadirvan <shadirvan2021@gmail.com>
2026-07-13 05:39:49 -0400

    Initial Commit

---
+++ common/protocol.go
@@ -0,0 +1,7 @@
+package common
+
+const (
+	RouteBeacon = "/api/v1/beacon"
+	RouteTasks  = "/api/v1/tasks"
+	RouteResult = "/api/v1/result"
+)
---
+++ go.mod
@@ -0,0 +1,5 @@
+module agni
+
+go 1.26.4
+
+require github.com/mattn/go-sqlite3 v1.14.48 // indirect
