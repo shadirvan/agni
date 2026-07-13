71767be87b034cae7d26b9ae5b2c89ab5d55299c
shadirvan <shadirvan2021@gmail.com>
2026-07-13 05:41:58 -0400

    Define Structure for task, task response, and beacon request

--- common/protocol.go
+++ common/protocol.go
@@ -5,3 +5,27 @@ const (
 	RouteTasks  = "/api/v1/tasks"
 	RouteResult = "/api/v1/result"
 )
+
+type BeaconRequest struct {
+	ID         string `json:"id"`
+	Hostname   string `json:"hostname"`
+	Username   string `json:"username"`
+	OS         string `json:"os"`
+	Arch       string `json:"arch"`
+	PID        int    `json:"pid"`
+	InternalIP string `json:"internal_ip"`
+}
+
+type Task struct {
+	ID      string `json:"id"`
+	Command string `json:"command"`
+	Args    string `json:"args"`
+	Timeout int    `json:"timeout"`
+}
+
+type TaskResponse struct {
+	TaskID  string `json:"task_id"`
+	Success bool   `json:"success"`
+	Output  string `json:"output"`
+	Error   string `json:"error"`
+}
